9f6c08078cdd67a459c168a3f5413e7501c8b510
Mahendran <98400425+mj6671@users.noreply.github.com>
2024-09-15 16:00:13 +0530

    Create GCR.py

---
+++ GCR.py
@@ -0,0 +1,21 @@
+
+#without recusin funtion:-
+def gcd(a,b):
+  if b==0:
+    return x
+  else:
+    retrun(x%y)
+
+x=int(input("enter the first number:")
+y=int(input("enter the second number:")
+print(num(x,y))    
+
+#with_recusion function:-
+   def gcd(a,b):
+      if b==0:
+        return x
+      else:
+         return gcd(y,x%y)
+x=int(input("enter the first number:")
+y=int(input("enter the second number:")
+print(num(x,y))  
